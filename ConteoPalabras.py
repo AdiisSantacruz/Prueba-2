7adec877d520992874c4d5714aa783decc1058e9
LAPTOP-NND7KTNQ\JUAN LOPEZ <charlie.lopez@epn.edu.ec>
2017-12-06 20:36:43 -0500

    bueno jovenes como ustedes son unos pro y han hecho yo guardo la respuesta xD

--- ConteoPalabras.py
+++ ConteoPalabras.py
@@ -1,3 +1,11 @@
+def creartxt():
+    archi=open('respuesta.txt','w')
+    archi.close()
+def grabartxt():
+    archi = open ('respuesta.txt','a')
+    archi.write(str(numPala))
+    archi.close()
+
 texto = "este texto es una prueba para el conteo de palabras xd"
 pala = texto.split()
 numPala = 0
@@ -6,3 +14,6 @@ while numPala < len(pala):
     numPala += 1
 
 print ("El numero de palabras en el texto es: " + str(numPala))
+
+creartxt()
+grabartxt()
